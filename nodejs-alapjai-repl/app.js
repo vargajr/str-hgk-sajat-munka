5707de159ce96ad12f5ca9cc350912c86da4f927
Varga Jenő <jeno_varga@yahoo.com>
2021-06-16 15:31:57 +0200

    project built, folders created, first lesson done

---
+++ nodejs-alapjai-repl/app.js
@@ -0,0 +1,19 @@
+// 1. Feladat
+// Ellenőrizd a telepített NodeJS verziószámát!
+// node -v
+
+// 2. Feladat
+// Ellenőrizd a telepített npm verziószámát!
+// npm -v
+
+// 3. Feladat
+// Listázd ki a projekted függőségeit! Csak az első szint kell, a telepített csomagok függőségei nem!
+// npm ls
+
+// 4. Feladat
+// Írasd ki a konzolra az eslint csomag adatait!
+// npm ls eslint
+
+// 5. Feladat
+// Kérdezd le az projekted elavult csomagjait, és ha van találat, frissítsd őket!
+// npm ls outdated
